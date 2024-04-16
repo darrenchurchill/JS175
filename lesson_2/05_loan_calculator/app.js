ba8c4d7df83731ccbbd996079b94303beedfd5ac
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-15 17:18:51 -0700

    2.6: Add CSS styles

--- lesson_2/05_loan_calculator/app.js
+++ lesson_2/05_loan_calculator/app.js
@@ -18,6 +18,33 @@ const HTML_START = `
   <head>
     <meta charset="utf-8">
     <title>Loan Calculator</title>
+    <style type="text/css">
+      body {
+        background: rgba(250, 250, 250);
+        font-family: sans-serif;
+        color: rgb(50, 50, 50);
+      }
+
+      article {
+        width: 100%;
+        max-width: 40rem;
+        margin: 0 auto;
+        padding: 1rem 2rem;
+      }
+
+      h1 {
+        font-size: 2.5rem;
+        text-align: center;
+      }
+
+      table {
+        font-size: 2rem;
+      }
+
+      th {
+        text-align: right;
+      }
+    </style>
   </head>
   <body>
     <article>
