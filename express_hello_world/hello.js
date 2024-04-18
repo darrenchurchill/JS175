45ba5ae951a94b9a7704d939439b212df439497e
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-18 12:54:56 -0700

    5.7: Add my own Common Log Format logger

--- express_hello_world/hello.js
+++ express_hello_world/hello.js
@@ -4,11 +4,34 @@
  * hello.js
  */
 const express = require("express");
+const strftime = require("strftime");
+
 const app = express();
 const PORT = 8080;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use((req, res, next) => {
+  // https://en.wikipedia.org/wiki/Common_Log_Format
+  let msg = [
+    req.ip,
+    "-", // RFC 1413 client identity
+    "-", // userid
+    `[${strftime("%d/%b/%Y:%H:%M:%S %z")}]`,
+    `"${req.method} ${req.path} HTTP/${req.httpVersion}"`,
+  ];
+
+  res.on("finish", () => {
+    console.log(
+      ...msg,
+      res.statusCode,
+      res.getHeader("Content-Length"),
+    );
+  });
+
+  next();
+});
 
 app.use(express.static("public"));
 
